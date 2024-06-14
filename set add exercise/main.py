b0a8b81de31bc0c4bf07dd6611493e1a57b0add3
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-14 18:09:08 +0300

    get data from input function

--- set add exercise/main.py
+++ set add exercise/main.py
@@ -1,3 +1,13 @@
+
+def get_data():
+    number_of_stamps = int(input().strip())
+    
+    data = []
+    for _ in number_of_stamps:
+        stamp = input().strip()
+        data.append(stamp)
+    return data
+
 def get_unique_stamps(stamps:list):
     unique_stamps = set(stamps)
     
