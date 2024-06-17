621d29a1760db2bf8f70034c6e65c398dbd14d6d
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-17 14:24:22 +0300

    read data method

--- Set-union-Operation/main.py
+++ Set-union-Operation/main.py
@@ -1,3 +1,10 @@
+def get_data():
+    english_sub_count = int(input().strip())
+    english_subsc = int(input().strip().split())
+    french_sub_count = int(input().strip())
+    french_subsc = int(input().strip().split())
+    
+    return english_subsc, french_subsc
 def get_subscibers_count(english, french):
     
     english = set(english)
