4996335e0ed93a59364b72fde734220be1b6117f
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-17 14:24:34 +0300

    main method

--- Set-union-Operation/main.py
+++ Set-union-Operation/main.py
@@ -1,3 +1,4 @@
+
 def get_data():
     english_sub_count = int(input().strip())
     english_subsc = int(input().strip().split())
@@ -5,10 +6,23 @@ def get_data():
     french_subsc = int(input().strip().split())
     
     return english_subsc, french_subsc
+
 def get_subscibers_count(english, french):
     
     english = set(english)
     french = set(french)
     
-    
+
     return len(english.union(french))
+
+
+def main():
+    
+    english_subscribers, french_subscribers = get_data()
+    
+    subscribers_count = get_subscibers_count(english_subscribers, french_subscribers)
+    print(subscribers_count)
+
+
+if __name__ == "__main__":
+    main()
