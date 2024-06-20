c906a4f063d5125549a57a1b62beef48b4955f26
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-06-20 18:20:36 +0300

    checks if a set is subset of set b

--- check subset/main.py
+++ check subset/main.py
@@ -9,3 +9,12 @@ def read_input_data():
         set_b = set([int(number) for number in input().strip().split()])
         tests.append({"a":set_a,"b":set_b})
     return tests
+
+
+
+def main():
+    tests = read_input_data()
+    for test in tests:
+        print(True) if test["a"].issubset(test["b"]) else print(False)
+if __name__ == "__main__":
+    main()    
