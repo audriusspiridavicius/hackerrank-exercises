7f10331d83f435b8c9971109c87d11360dedab8a
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-01 00:31:21 +0300

    beautifulDays function

--- Beautiful Days at the Movies/index.py
+++ Beautiful Days at the Movies/index.py
@@ -7,4 +7,18 @@ def is_beautiful_day(day,k):
 
     return beautiful_day
 
+def beautifulDays(i, j, k):
+    
+    beautiful_days_count = 0
+    
+    for day in range(i,j+1):
+        if is_beautiful_day(day,k):
+            beautiful_days_count += 1
+            
+    return beautiful_days_count
+    
+    
+    
+    
 if __name__ == "__main__":
+    print(beautifulDays(1,15,3))
