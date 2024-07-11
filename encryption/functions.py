6f392ace2607cfd350d24ba329c39f234924330b
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-11 21:01:16 +0300

    get_encrypted_text refactored. unnecessary functions removed.

--- encryption/functions.py
+++ encryption/functions.py
@@ -4,38 +4,27 @@ def remove_spaces(text:str):
     return text.replace(" ","")
 
 
-def get_grid_dimensions(number:int):
+def get_enceyption_step(number:int):
     
     sqrt = math.sqrt(number)
     
-    rows = math.floor(sqrt)
-    columns = math.ceil(sqrt)
+    step = math.ceil(sqrt)
     
-    return rows, columns
+    return step
 
-
-def text_to_grid(text, rows, columns):
+def get_encrypted_text(text:str):
     
-    grid_array = []
-    column = 0
-    for _ in range(columns):
-        row = [*text[column:column+columns]]
-        if row:
-            grid_array.append(row)
-        column += columns
+    encrypted_text = ""
     
-    return grid_array
-
-
-def get_encrypted_text(grid:list):
+    step = get_enceyption_step(len(text))
+    first_row = text[0:step]
+    word_separator = " "
     
-    encrypted_text = []
-    for row_index in range(len(grid[0])):
-        wrd = ""
-        for index in range(len(grid)):
-            if row_index < len(grid[index]):
-                wrd += grid[index][row_index]
-        encrypted_text.append(wrd)
-    
-    return " ".join(encrypted_text)
-            
+    for letter_index,_ in enumerate(first_row):
+        index = letter_index
+        while index < len(text):
+            encrypted_text += text[index]
+            index += step
+        encrypted_text += word_separator
+    return encrypted_text.strip()
+                
