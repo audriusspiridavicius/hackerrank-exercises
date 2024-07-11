b2b08ea02d3bf96831d771abf14f5bf58cbe9406
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-11 21:01:31 +0300

    encryption function refactor

--- encryption/index.py
+++ encryption/index.py
@@ -1,15 +1,8 @@
-from functions import remove_spaces, get_grid_dimensions, text_to_grid, get_encrypted_text
+from functions import remove_spaces, get_encrypted_text
 
 def encryption(text):
     
     no_spaces_text = remove_spaces(text)
     
-    text_length = len(no_spaces_text)
-    
-    rows, columns = get_grid_dimensions(text_length)
-    
-    
-    grid = text_to_grid(no_spaces_text, rows, columns)
-    
-    enc_text = get_encrypted_text(grid)
+    enc_text = get_encrypted_text(no_spaces_text)
     return enc_text
