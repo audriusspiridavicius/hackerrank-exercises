da03e0068f57aec5b00ab2679a6506bc72559f8a
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-12 22:42:16 +0300

    appendAndDelete refactor

--- append_and_delete/index.py
+++ append_and_delete/index.py
@@ -12,23 +12,36 @@ def appendAndDelete(s:str, t:str, k:int):
         else:
             break
     
+
     operations_count = 0
     
     if match_index == -1:
         operations_count = s_length + t_length
+    elif (match_index + 1 == s_length and match_index + 1 == t_length):
+        if k % 2 == 0:
+            operations_count = k
+        match_index = -1
+    elif match_index == 0 and t_length == 1:
+        operations_count = k
     elif match_index > -1:
         operations_count = (t_length - (match_index + 1)) + (s_length - (match_index + 1))
-    
-    if operations_count <= k:
+        
+
+
+    if operations_count == k or (operations_count <= k and match_index == -1) or (operations_count < k and match_index > -1 and (k - operations_count ) % 2 ==0):
         return "Yes"
     else:
         return "No"
     
     
 if __name__ == "__main__":
-    print(appendAndDelete("abc","dcf",5))
-    print(appendAndDelete("abc","acf",3))
-    print(appendAndDelete("hackerhappy","hackerrank",10))
-    print(appendAndDelete("cackerhappy","hackerrank",21))
+    print(appendAndDelete("aaa","aaa",10))
+    print(appendAndDelete("aaa","a",5))
+    print(appendAndDelete("a","ab",2))
+    # print(appendAndDelete("abc","acf",3))
+    # print(appendAndDelete("hackerhappy","hackerrank",9))
+    # print(appendAndDelete("cackerhappy","hackerrank",21))
+    print(appendAndDelete("abcd","abcdert",10))
+    print(appendAndDelete("aaaaaaaaaa","aaaaa",7))
 
         
