be2057a6f81e6c436463fbc4da0b30bdb35bc5e4
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-16 17:13:35 +0300

    get_astronauts_by_countries. get all astronauts for specific country

--- jorney_to_the_moon/index.py
+++ jorney_to_the_moon/index.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 def form_related_astronauts(astronauts:list) -> defaultdict:
         
         astronauts_dict = defaultdict(set)
@@ -7,3 +8,16 @@ def form_related_astronauts(astronauts:list) -> defaultdict:
             astronauts_dict[astronaut[1]].add(astronaut[0])
         
         return astronauts_dict
+    
+def get_astronauts_by_countries(n, astronauts):
+    
+    by_countries = []
+    visited_astronauts = [False] * n
+    for astronaut_number in range(n):
+        same_country_astronauts = set()
+        if astronauts.get(astronaut_number) and not visited_astronauts[astronaut_number]:
+            same_country_astronauts = get_related_astronauts(astronaut_number, astronauts)
+            visited_astronauts = [True if index in same_country_astronauts else value for index,value in enumerate(visited_astronauts)]
+            by_countries.append(same_country_astronauts)
+    return by_countries
+
