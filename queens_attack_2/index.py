6bf17960484b2a8eb67ea52938853868dfa27e1e
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-23 14:26:02 +0300

    queensAttack function completed

--- queens_attack_2/index.py
+++ queens_attack_2/index.py
@@ -27,3 +27,29 @@ def get_board_squares(queen_position:tuple[int,int], board_size:int, row_step, c
     return square_list
         
         
+def queensAttack(n, k, r_q, c_q, obstacles):
+    
+    obstacles = set(obstacles)
+    queen_attack_squares = []
+    
+    queen_position = (r_q, c_q)
+    
+    queen_attack_squares.append(get_board_squares(queen_position, n, 0, 1, obstacles)) # queens position right
+    queen_attack_squares.append(get_board_squares(queen_position, n, 0, -1, obstacles)) # queens position left
+    queen_attack_squares.append(get_board_squares(queen_position, n, 1, 0, obstacles)) # queens position up
+    queen_attack_squares.append(get_board_squares(queen_position, n, -1, 0, obstacles)) # queens position down
+    
+    queen_attack_squares.append(get_board_squares(queen_position, n, 1, 1, obstacles)) # queens position diagonal up right
+    queen_attack_squares.append(get_board_squares(queen_position, n, 1, -1, obstacles)) # queens position diagonal up left
+    
+    queen_attack_squares.append(get_board_squares(queen_position, n, -1, -1, obstacles)) # queens position diagonal down left
+    queen_attack_squares.append(get_board_squares(queen_position, n, -1, 1, obstacles)) # queens position diagonal down right
+    
+    queen_attack_squares = sum([len(values) for values in queen_attack_squares])
+    
+    return queen_attack_squares
+
+if __name__ == "__main__":
+    print(f"result = {queensAttack(5,3,4,3,[(5,5),(4,2),(2,3)])}")
+    print(f"result = {queensAttack(4,0,4,4,[])}")
+   
